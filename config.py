339a8cef42b5898f8fd8a3c19f6009004353fa75
Reed Murphy <reed@reedmurphy.net>
2013-02-08 12:53:09 +1100

    vim movement keys

--- config.py
+++ config.py
@@ -9,4 +9,12 @@ class Config:
         'KEY_RIGHT': Bind.MOVE_E,
         'KEY_DOWN': Bind.MOVE_S,
         'KEY_LEFT': Bind.MOVE_W,
+        'h': Bind.MOVE_W,
+        'j': Bind.MOVE_S,
+        'k': Bind.MOVE_N,
+        'l': Bind.MOVE_E,
+        'y': Bind.MOVE_NW,
+        'u': Bind.MOVE_NE,
+        'b': Bind.MOVE_SW,
+        'n': Bind.MOVE_SE,
     }
